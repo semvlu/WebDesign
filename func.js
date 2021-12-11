deb174ff8ae23adaa6ba13465857acf538c179e7
semvlu <74308718+semvlu@users.noreply.github.com>
2021-12-12 02:49:18 +0800

    Add files via upload

---
+++ func.js
@@ -0,0 +1,16 @@
+function myFunc() {
+    document.getElementById("demo").innerHTML = "aNyWaY";
+}
+
+function swColor(btn, color) {
+    var count = 1;
+    var property = document.getElementById(btn);
+    if (count == 0){
+        property.style.backgroundColor = "#FFFFFF"
+        count=1;        
+    }
+    else{
+        property.style.backgroundColor = "#7FFF00"
+        count=0;
+    }
+}
